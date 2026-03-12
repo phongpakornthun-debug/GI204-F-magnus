8ce95e4415aad54299fbc98ec7542d46f742c2d1
phongpakornthun-debug <phongpakorn.thun@bumail.net>
2026-03-12 10:34:59 +0700

    add scrip

---
+++ Assets/Scrip/TorqueRotate.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueRotate : MonoBehaviour
+{
+    public float torque;
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(new Vector3(torque, 0, 0));  
+        }
+    }
+}
